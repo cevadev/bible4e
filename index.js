1471afd2041ed4b488caf4fd1d667e629ec4961d
cevadev <carlos.villanueva.altuna@gmail.com>
2023-07-01 00:26:31 -0300

    Project Bible for Everyone (Bible4e)

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.info('Primeros pasos con el proyecto Bible for everyone (bible4e)');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/verse', (req, res) => {
+  res.json({
+    verse: 'Juan 3:16',
+    text: 'Porque de tal manera Dios al mundo que ha dado a su hijo unigenito para que todo aquel que el crea no se pierda mas tenga vida eterna',
+  });
+});
+
+app.listen(port, () => {
+  console.info('La aplicacion se esta ejecutando satisfactoriamente');
+});
